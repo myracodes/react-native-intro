07fe3bc922b551ba65181c8d9925b1b6fa2dcc51
Myriam Mira <77079498+myracodes@users.noreply.github.com>
2022-02-16 14:14:34 +0100

    Finish quest 2

--- App.js
+++ App.js
@@ -1,11 +1,22 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View, Button } from "react-native";
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text style={{ color: "black" }}>Youpi ça marche :D</Text>
       <StatusBar style="auto" />
+      <Text style={{ color: "yellow" }}>Et un autre !</Text>
+      <Button
+        // onPress={onPressAction}
+        title="Click me"
+        color="#841584"
+        accessibilityLabel="This is a test button"
+        s
+      />
+      <Text style={{ color: "#FFF" }}>Et encore un !</Text>
+      <Text style={{ color: "#FFF" }}>Et encore un !</Text>
+      <Text style={{ color: "#FFF" }}>Et encore un !</Text>
     </View>
   );
 }
@@ -13,8 +24,8 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#f0f",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
